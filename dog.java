4c8b5c9b4f5f23d8b032ba0f49442469328077da
sheharaashraf <76677381+sheharaashraf@users.noreply.github.com>
2021-05-07 12:14:13 +0530

    Add files via upload

---
+++ dog.java
@@ -0,0 +1,33 @@
+class Dog
+{
+//Instance variable
+String name;
+int age;
+//constructor Declaration of class
+public Dog(String name,int age)
+{
+ 	this.name = name;
+	 this.age = age;
+}	
+
+public void getName()
+{
+	System.out.println(name);
+}
+public void getAge()
+{
+	System.out.println (age);
+}
+public static void main(String a[])
+{
+ 	Dog tuffy = new Dog("tuffy",5);
+ 	Dog tuffy1 = new Dog("tuffy1",4);
+ 	Dog tuffy2 = new Dog("tuffy2",15);
+ 	tuffy.getName();
+ 	tuffy.getAge();
+ 	tuffy1.getName();
+ 	tuffy1.getAge();
+ 	tuffy2.getName();
+ 	tuffy2.getAge();
+}
+}
